4c7f7d1b34e984956dee76eb65769b85cce701bc
Eric P. Katz <ericpkatz@gmail.com>
2018-11-03 13:55:28 -0400

    add spread

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,10 @@ module.exports = {
       {
         exclude: /node_modules/,
         test: /\.js$/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
+                query: {
+          plugins:[ 'transform-object-rest-spread' ]
+        }
       }
     ]
   }
